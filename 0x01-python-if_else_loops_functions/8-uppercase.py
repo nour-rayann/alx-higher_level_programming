338e730582503154015ddc94a6ad39ff30ae850b
Nouran <no0ouranmohamed@gmail.com>
2023-11-01 03:01:55 +0200

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        c = str[i]
+        if ord(c) > 96 and ord(c) < 123:
+            c = chr(ord(c) - 32)
+        if i < (len(str) - 1):
+            print(c, end="")
+        else:
+            print(c)
