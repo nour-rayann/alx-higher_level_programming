533899be474d530dd173683082fcc7107157bf21
Nouran <no0ouranmohamed@gmail.com>
2023-11-01 03:26:25 +0200

    task 12 fixed

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-for i in range(0, 101):
-    copy = i
-    if copy % 3 == 0 and copy % 5 == 0:
-        print("FizzBuzz", end=" ")
-    elif copy % 3 == 0:
-        print("Fizz", end=" ")
-    elif copy % 5 == 0:
-        print("Buzz", end=" ")
-    else:
-        print("{:d}".format(i), end=" ")
+def fizzbuzz():
+    for i in range(0, 101):
+        copy = i
+        if copy % 3 == 0 and copy % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif copy % 3 == 0:
+            print("Fizz", end=" ")
+        elif copy % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
